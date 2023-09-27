e75ad5f69676346cea185ac11de1c6d34f693651
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-09-27 11:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function calculateMinCost() {
   //your code here
+	let  ropes = document.getElementById("rope-lengths").value
+let arr = ropes.split(",");
+let finalValue =0;
+while(arr.length>1){
+arr.sort((a,b) =>{return (a-b)});
+let value = parseInt(arr.shift());
+let value1 = parseInt(arr.shift());
+let mainValue = value+value1;
+finalValue = finalValue+mainValue;
+arr.push(mainValue);
+}
+let result = document.getElementById("result");
+result.innerText = finvalValue;
+return finalValue;
   
   
   
